7f2f28c9dd8754324125ecc5316b75f531896098
Dr Robert Lowe <robert@wht-che-8770.smd.qmul.ac.uk>
2019-03-25 15:20:44 +0000

    Preliminary Version

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from sys import argv
+import os
+import pysam
 
 def getopts(argv):
     opts = {}  # Empty dictionary to store key-value pairs.
@@ -9,9 +11,12 @@ def getopts(argv):
     return opts
 
 def printHelp():
-    print("Usage: windowcount -g <chromosome sizes>")
+    print("Usage: windowcount -g <chromosome sizes> -d <directory of BAMs>")
     print("Options:")
     print("\t-g\t\tSet the genome chromosome sizes")
+    print("\t-d\t\tDirectory to search for BAM files")
+    print("\t-w\t\tSize of window [Int]")
+    print("\t-s\t\tAmount of shift between each window [Int]")
     exit()
 
 def parseChromSizes(filename):
@@ -22,13 +27,52 @@ def parseChromSizes(filename):
         out.append([c,l])
     return(out)
     
+def extractreadcounts(chrom,spos,epos,sample):
+    count=0
+    for read in sample.fetch(chrom, spos, epos):
+        count+=1
+    return count
+    
+def windowcount(chromsizes,size,sep,files):
+    l=chromsizes[1]
+    p=0
+    #Only allow complete windows starting from 0
+    while (p+size)<l:
+        counts=[]
+        for f in files:
+            counts.append(extractreadcounts(chromsizes[0],p,p+size,f))
+        if sum(counts)>0:
+            string=f"{chromsizes[0]}\t{p}\t{p+size}"
+            for c in counts:
+                string+=f"\t{c}"
+            print(string)
+        p+=sep
+    exit()
+    
+def getfiles(direc):
+    out=[]
+    header="chrom\tspos\tepos"
+    for i in os.listdir(direc):
+        if i.find(".bam")!=-1 and i.find(".bai")==-1:
+            out.append(pysam.AlignmentFile(f"{direc}/{i}", "rb"))
+            header+=f"\t{i.split('.bam')[0]}"
+    print(header)
+    return(out)
+        
 def main():
     myargs = getopts(argv)
     if '-g' in myargs: 
         ref=myargs["-g"]
     else:
         printHelp()
+    if '-d' in myargs: 
+        direc=myargs["-d"]
+    else:
+        printHelp()
     cs=parseChromSizes(ref)
+    files=getfiles(direc)
+    for k in cs:
+        windowcount(k,100,50,files)
         
 if __name__=="__main__":
     main()
