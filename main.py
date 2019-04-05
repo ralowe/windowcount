03ab0391c3b9d605b2fc1967080ec81bd472515b
Dr Robert Lowe <robert@wht-che-8770.smd.qmul.ac.uk>
2019-04-05 11:27:23 +0100

    Moved to more effecient way of itereating through sorted file

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 from sys import argv
+from sys import stderr
 import os
 import pysam
+import time
+
+class BamFile:
+    def __init__(self,filename):
+        self.itr = pysam.AlignmentFile(filename, "rb").fetch()
+        self.readnext()
+    
+    def currentRead(self):
+        return(self.r)
+        
+    def readnext(self):
+        while True:
+            self.r=next(self.itr)
+            if self.r.next_reference_start>self.r.reference_start:
+                self.frag=[self.r.reference_name,self.r.reference_start,self.r.reference_start+self.r.template_length,self.r.query_name]
+                break
+        
+    def getfragment(self):
+        return self.frag
+        
 
 def getopts(argv):
     opts = {}  # Empty dictionary to store key-value pairs.
@@ -20,43 +41,62 @@ def printHelp():
     exit()
 
 def parseChromSizes(filename):
-    out=[]
+    cs=[]
+    ls=[]
     for k in open(filename):
         c=k.split("\t")[0]
         l=int(k.split("\t")[1])
-        out.append([c,l])
-    return(out)
+        cs.append(c)
+        ls.append(l)
+    return(cs,ls)
     
-def extractreadcounts(chrom,spos,epos,sample):
+def extractreadcounts(chrom,spos,epos,sample,buffer):
+    #Check we havent gone to the next chromosome
+    while (sample.frag[1]<epos and chrom==sample.frag[0]):
+        buffer.append(sample.frag)
+        sample.readnext()
+    
+    for k in range(len(buffer)-1,-1,-1):
+        if buffer[k][2]<spos:
+            del buffer[k]
+    
     count=0
-    for read in sample.fetch(chrom, spos, epos):
-        count+=1
-    return count
+    if buffer:
+        count+=len(buffer)
+    return buffer,count
     
-def windowcount(chromsizes,size,sep,files):
-    l=chromsizes[1]
+def windowcount(chrom,l,size,sep,files,outfile):
     p=0
+    readbuffers=[[]]*len(files)
     #Only allow complete windows starting from 0
     while (p+size)<l:
         counts=[]
-        for f in files:
-            counts.append(extractreadcounts(chromsizes[0],p,p+size,f))
+        for z in range(len(files)):
+            readbuffers[z],c1 = extractreadcounts(chrom,p,p+size,files[z],readbuffers[z])
+            counts.append(c1)
         if sum(counts)>0:
-            string=f"{chromsizes[0]}\t{p}\t{p+size}"
+            string=f"{chrom}\t{p}\t{p+size}"
             for c in counts:
                 string+=f"\t{c}"
-            print(string)
+            outfile.write(f"{string}\n")
         p+=sep
     
-def getfiles(direc):
-    out=[]
+def checkchrom(cs,current,files):
+    #Move to the right chromosome
+    for z in files:
+        while(cs.index(z.frag[0])<current):
+            z.readnext()
+
+    
+def getfiles(direc,outfile):
+    files=[]
     header="chrom\tspos\tepos"
     for i in os.listdir(direc):
         if i.find(".bam")!=-1 and i.find(".bai")==-1:
-            out.append(pysam.AlignmentFile(f"{direc}/{i}", "rb"))
+            files.append(BamFile(f"{direc}/{i}"))
             header+=f"\t{i.split('.bam')[0]}"
-    print(header)
-    return(out)
+    outfile.write(f"{header}\n")
+    return(files)
         
 def main():
     myargs = getopts(argv)
@@ -74,10 +114,15 @@ def main():
         w=int(myargs["-w"])
     if '-s' in myargs: 
         s=int(myargs["-s"])
-    cs=parseChromSizes(ref)
-    files=getfiles(direc)
-    for k in cs:
-        windowcount(k,w,s,files)
-        
+    cs,ls=parseChromSizes(ref)
+    outfile=open("test.txt","w")
+    files=getfiles(direc,outfile)
+    for k in range(len(cs)):
+        print("*******************")
+        print(k)
+        t=time.time()
+        checkchrom(cs,k,files)
+        windowcount(cs[k],ls[k],w,s,files,outfile)
+        stderr.write(f"Done {cs[k]} in {time.time()-t}s\n")
 if __name__=="__main__":
     main()
